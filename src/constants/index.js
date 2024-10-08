65e0b767d86c542d9574c1c986322882520af751
unknown <intesedzel@gmail.com>
2024-10-08 15:05:22 +0800

    update experience and projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
+import citizenOne from "../assets/projects/citizenOne.png";
 import project1 from "../assets/projects/iweft.png";
 import project2 from "../assets/projects/car-rental.png";
 import project3 from "../assets/projects/trends.png";
@@ -12,7 +13,22 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "May 2024 - Present",
+    year: "August 2024 - October 2024",
+    role: "Front-end Mobile Developer",
+    company: "Awork Company Denmark",
+    description: `I developed a comprehensive mobile application for a citizen record system in Denmark. This app provides a complete digital solution for electronic registration, secure data storage, and easy access to relevant information for authorized users. The system eliminates the need for physical documents or scattered digital files, ensuring that all necessary data is available when needed. The app was built using React Native, TypeScript, and Expo. Implemented RESTful APIs with a Laravel backend and integrated with MySQL databases.`,
+    technologies: [
+      "Typescript",
+      "React Native",
+      "Expo",
+      "Expo Router",
+      "Mysql Database",
+      "Postman",
+      "Laravel",
+    ],
+  },
+  {
+    year: "May 2024 - August 2024",
     role: "Front-end Mobile Developer",
     company: "PH Virtual Team.",
     description: `I developed a comprehensive mobile application for booking laundry services in Dubai. This app streamlines the process of scheduling, managing, and tracking laundry services for users, providing a convenient solution for both customers and laundry service providers using JavaScript, React Native, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases.`,
@@ -63,7 +79,27 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "iWeft - Booking Laundry Mobile Application - Dubai",
+    title:
+      "CitizenOne™ - Mobile Application for Citizen Record System - (Denmark)",
+    image: citizenOne,
+    description:
+      "Developed a fully functional mobile application for a citizen record system in Denmark. This digital solution allows for electronic registration and secure storage of all relevant data on a user-friendly platform. The system features a central database, enabling authorized users to access critical information quickly and easily without dealing with physical documents or scattered digital files.",
+    technologies: [
+      "React Native",
+      "Typescript",
+      "Mysql",
+      "Laravel",
+      "Expo",
+      "Expo Router",
+    ],
+    // apple:
+    //   "https://apps.apple.com/us/app/iweft-laundry-dry-cleaning/id6578443052?platform=iphone",
+    apple: "",
+    google: "",
+    github: "https://github.com/AWORK-AS/citizenone-mobile",
+  },
+  {
+    title: "iWeft - Booking Laundry Mobile Application - (Dubai)",
     image: project1,
     description:
       "A fully functional mobile application for booking laundry services in Dubai. This app streamlines the process of scheduling, managing, and tracking laundry services for users, providing a convenient solution for both customers and laundry service providers using JavaScript, React Native, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases.",
@@ -81,7 +117,7 @@ export const PROJECTS = [
     github: "https://github.com/inteszel143/iweft",
   },
   {
-    title: "iMotor.app - Car Listing Mobile Application - Dubai",
+    title: "iMotor.app - Car Listing Mobile Application - (Dubai)",
     image: project2,
     description:
       "A fully functional mobile application for listing and exploring various vehicles, including cars, motorcycles, trucks, and boats, all based in Dubai. This app provides a user-friendly platform for both buyers and sellers to interact and conduct transactions seamlessly.",
@@ -100,7 +136,7 @@ export const PROJECTS = [
     github: "https://github.com/inteszel143/iMotor",
   },
   {
-    title: "TrendingCast - Social Media Clone Mobile Application",
+    title: "TrendingCast - Social Media Clone Mobile Application (Ojt project)",
     image: project3,
     description:
       "A social media app where I've taken inspiration from Twitter, featuring a user-friendly design reminiscent of your favorite short-form blogging platform.",
@@ -110,7 +146,7 @@ export const PROJECTS = [
     github: "https://github.com/inteszel143/Trending-cast",
   },
   {
-    title: "Davcu - E-commerce Mobile Application - Capstone Project",
+    title: "Davcu - E-commerce Mobile Application - (Capstone Project)",
     image: project4,
     description:
       "A mobile e-commerce app for native handicrafts in Davao City that allows users to purchase native products and promote our handmade, local products. This app caters to three types of users: buyers, sellers, and riders, and it also provides an interface for searching, comparing, and tracking orders.",
